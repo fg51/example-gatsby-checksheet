312f678e10883c22cc633e6698359e76dcfd1cbe
kflange <zknsftk1msrk@gmail.com>
2020-12-14 18:05:19 +0900

    Added: form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import React from "react"
+import { useState } from "react"
 
 import {
+  IonButton,
   IonCard,
   IonCardTitle,
   IonCardContent,
+  IonContent,
   IonListHeader,
   IonItem,
   IonLabel,
@@ -12,7 +15,7 @@ import {
   IonPage,
   IonTitle,
   IonToggle,
-  IonIcon,
+  IonHeader,
 } from "@ionic/react"
 
 import "@ionic/react/css/core.css"
@@ -23,75 +26,95 @@ import "@ionic/react/css/normalize.css"
 import "@ionic/react/css/padding.css"
 import "@ionic/react/css/text-alignment.css"
 
-import { pencil } from "ionicons/icons"
-
 import { InputSerialMs, InputSerialPump } from "../components/InputSerialMs"
 
 import { IconPencil } from "../components/IconPencil"
 
 export default function Home() {
+  const [text, setText] = useState<string>("")
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    alert("A name was submitted: " + text)
+    event.preventDefault()
+  }
+
   return (
     <IonPage>
-      <IonTitle> CHECKSHEET </IonTitle>
+      <IonHeader>
+        <IonTitle> CHECKSHEET </IonTitle>
+        <IonList>
+          <IonItem>
+            <IonLabel>製造番号</IonLabel> <InputSerialMs />
+            <IconPencil />
+          </IonItem>
+          <IonItem>
+            <IonLabel>main 製造番号</IonLabel> <InputSerialPump />
+            <IconPencil />
+          </IonItem>
+        </IonList>
+      </IonHeader>
 
-      <IonList>
-        <IonItem>
-          <IonLabel>製造番号</IonLabel> <InputSerialMs />
-          <IconPencil />
-        </IonItem>
-        <IonItem>
-          <IonLabel>main 製造番号</IonLabel> <InputSerialPump />
-          <IconPencil />
-        </IonItem>
-      </IonList>
+      <IonContent class="ion-padding" scrollEvents={true}>
+        <IonList>
+          <IonListHeader>メインベース組立</IonListHeader>
 
-      <IonList>
-        <IonListHeader>メインベース組立</IonListHeader>
+          <IonCard>
+            <IonCardTitle>締結</IonCardTitle>
+            <IonCardContent>
+              <IonList>
+                <IonItem>
+                  <IonLabel>加工日</IonLabel> <IonInput />
+                  <IconPencil />
+                </IonItem>
+                <IonItem>
+                  <IonLabel>ベース製造番号</IonLabel> <IonInput />
+                  <IconPencil />
+                </IonItem>
+                <IonItem>
+                  <IonToggle />
+                  <IonLabel>止めねじ</IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonToggle />
+                  <IonLabel>80degC昇温</IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonToggle />
+                  <IonLabel>冷却</IonLabel>
+                </IonItem>
+                <IonItem>
+                  <IonLabel>モータ製造番号</IonLabel>
+                  <IonInput />
+                  <IconPencil />
+                </IonItem>
+              </IonList>
+            </IonCardContent>
+          </IonCard>
 
-        <IonCard>
-          <IonCardTitle>締結</IonCardTitle>
-          <IonCardContent>
-            <IonList>
-              <IonItem>
-                <IonLabel>加工日</IonLabel> <IonInput />
-                <IconPencil />
-              </IonItem>
-              <IonItem>
-                <IonLabel>ベース製造番号</IonLabel> <IonInput />
-                <IconPencil />
-              </IonItem>
+          <IonCard>
+            <IonCardTitle>ベアリング組み込み</IonCardTitle>
+            <IonCardContent>
               <IonItem>
                 <IonToggle />
-                <IonLabel>止めねじ</IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonToggle />
-                <IonLabel>80degC昇温</IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonToggle />
-                <IonLabel>冷却</IonLabel>
-              </IonItem>
-              <IonItem>
-                <IonLabel>モータ製造番号</IonLabel>
-                <IonInput />
+                <IonLabel>ベアリング製造番号</IonLabel> <IonInput />
                 <IconPencil />
               </IonItem>
-            </IonList>
-          </IonCardContent>
-        </IonCard>
+            </IonCardContent>
+          </IonCard>
+        </IonList>
 
-        <IonCard>
-          <IonCardTitle>ベアリング組み込み</IonCardTitle>
-          <IonCardContent>
-            <IonItem>
-              <IonToggle />
-              <IonLabel>ベアリング製造番号</IonLabel> <IonInput />
-              <IconPencil />
-            </IonItem>
-          </IonCardContent>
-        </IonCard>
-      </IonList>
+        <form className="ion-padding" onSubmit={handleSubmit}>
+          <IonLabel>Name:</IonLabel>
+          <IonInput
+            type="text"
+            value={text}
+            onIonChange={event => setText(event.detail.value!)}
+          />
+          <IonButton className="ion-margin-top" type="submit" expand="block">
+            送信
+          </IonButton>
+        </form>
+      </IonContent>
     </IonPage>
   )
 }
