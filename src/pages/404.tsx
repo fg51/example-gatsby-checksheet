03a0c24849c86d84d6ac54823e1c48b959a60bf0
kflange <zknsftk1msrk@gmail.com>
2020-12-14 14:25:28 +0900

    Added: 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import { IonPage } from "@ionic/react"
+
+const NotFound: React.FC = () => {
+  return (
+    <IonPage>
+      <p>アクセスしたページは存在しません。</p>
+    </IonPage>
+  )
+}
+
+export default NotFound
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import React from "react"
 
-import { IonInput, IonItem } from '@ionic/react';
+import { IonInput, IonItem, IonPage, IonTitle } from "@ionic/react"
 
 export default function Home() {
   return (
-    <div style={{ margin: "2em"}}>
-    Hello Gatsby with ionic
+    <IonPage>
+      <IonTitle> Hello Gatsby with ionic </IonTitle>
       <IonItem>
-        <IonInput value="sample" placeholder="Enter Input"> </IonInput>
+        <IonInput value="sample" placeholder="Enter Input">
+          {" "}
+        </IonInput>
       </IonItem>
-    </div>
-  );
+    </IonPage>
+  )
 }
-
